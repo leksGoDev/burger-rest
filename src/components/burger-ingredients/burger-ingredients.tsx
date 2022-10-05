2fd9704c1a71fcda4be8ab1e50391ac70060b93f
leksGo <Gan-96@yandex.ru>
2022-10-05 23:58:52 +0300

    add sauces section and scrollbar

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -4,6 +4,10 @@ import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from './burger-ingredients.module.css'
 import bun1 from '../../images/bun-01.png';
 import bun2 from '../../images/bun-02.png';
+import sauce1 from '../../images/sauce-01.png';
+import sauce2 from '../../images/sauce-02.png';
+import sauce3 from '../../images/sauce-03.png';
+import sauce4 from '../../images/sauce-04.png';
 import BurgerIngredientsCard from "./burger-ingredients-card/burger-ingredients-card";
 
 enum Tabs {
@@ -17,7 +21,7 @@ const BurgerIngredients = () => {
 
     return (
         <article>
-            <section className="mt-10 mb-10">
+            <nav className="mt-10 mb-10">
                 <p className="text text_type_main-large mb-5">Соберите бургер</p>
 
                 <div style={{ display: "flex" }}>
@@ -31,31 +35,59 @@ const BurgerIngredients = () => {
                         Начинки
                     </Tab>
                 </div>
+            </nav>
+
+            <section className={styles.content}>
+                <section className="mb-10">
+                    <p className="text text_type_main-medium mb-6">Булки</p>
+
+                    <ol className={`${styles.list} pl-4 pr-2`}>
+                        <BurgerIngredientsCard
+                            image={bun2}
+                            price={20}
+                            name="Краторная булка N-200i"
+                            count={1}
+                        />
+
+                        <BurgerIngredientsCard
+                            image={bun1}
+                            price={20}
+                            name="Флюоресцентная булка R2-D3"
+                        />
+                    </ol>
+                </section>
+
+                <section className="mb-10">
+                    <p className="text text_type_main-medium mb-6">Соусы</p>
+
+                    <ol className={`${styles.list} pl-4 pr-2`}>
+                        <BurgerIngredientsCard
+                            image={sauce2}
+                            price={30}
+                            name="Соус Spicy-X"
+                        />
+
+                        <BurgerIngredientsCard
+                            image={sauce4}
+                            price={30}
+                            name="Соус фирменный Space Sauce"
+                        />
+
+                        <BurgerIngredientsCard
+                            image={sauce3}
+                            price={30}
+                            name="Соус Spicy-X"
+                            count={1}
+                        />
+
+                        <BurgerIngredientsCard
+                            image={sauce1}
+                            price={30}
+                            name="Соус фирменный Space Sauce"
+                        />
+                    </ol>
+                </section>
             </section>
-
-            <section className="mb-10">
-                <p className="text text_type_main-medium mb-6">Булки</p>
-
-                <ol className={`${styles.cardSection} pl-4 pr-4`}>
-                    <BurgerIngredientsCard
-                        image={bun2}
-                        price={20}
-                        name="Краторная булка N-200i"
-                        count={1}
-                    />
-
-                    <BurgerIngredientsCard
-                        image={bun1}
-                        price={20}
-                        name="Флюоресцентная булка R2-D3"
-                    />
-                </ol>
-            </section>
-
-            <section className="mt-10 mb-10">
-
-            </section>
-
         </article>
     );
 };
