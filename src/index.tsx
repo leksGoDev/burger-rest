57a2e7b5d7c55db6d31a1e085319f2f30d51b942
leksGo <Gan-96@yandex.ru>
2022-10-24 22:24:10 +0300

    add redux and create store

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import './index.css';
+import { store } from "./services/store";
 import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +11,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
