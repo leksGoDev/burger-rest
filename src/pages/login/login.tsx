7f11c3812a47d162b757d3183fba1faf2c4f8c40
leksGo <Gan-96@yandex.ru>
2022-11-03 23:10:19 +0300

    add login page content

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,67 @@
 import type { FC } from 'react';
+import { EmailInput, PasswordInput, Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./login.module.css";
 
 const Login: FC = () => {
-    return (
-        <div>
 
-        </div>
+    return (
+        <main className={styles.main}>
+            <form className={`${styles.form} mb-20`}>
+                <p className="text text_type_main-medium">
+                    Вход
+                </p>
+
+                <EmailInput
+                    name='email'
+                    isIcon={false}
+                    value={""}
+                    onChange={() => null}
+                />
+
+                <PasswordInput
+                    name='password'
+                    value={""}
+                    onChange={() => null}
+                />
+                
+                <Button
+                    type="primary"
+                    htmlType="submit"
+                >
+                    Войти
+                </Button>
+            </form>
+
+            <aside className={styles.aside}>
+                <div className={styles.linkWrap}>
+                    <p className="text text_type_main-default text_color_inactive">
+                        Вы — новый пользователь?
+                    </p>
+
+                    <Button
+                        type="secondary"
+                        htmlType="button"
+                        style={{ padding: 0 }}
+                    >
+                        Зарегистрироваться
+                    </Button>
+                </div>
+
+                <div className={styles.linkWrap}>
+                    <p className="text text_type_main-default text_color_inactive">
+                        Забыли пароль?
+                    </p>
+
+                    <Button
+                        type="secondary"
+                        htmlType="button"
+                        style={{ padding: 0 }}
+                    >
+                        Восстановить пароль
+                    </Button>
+                </div>
+            </aside>
+        </main>
     );
 };
 
