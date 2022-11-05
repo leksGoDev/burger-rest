4e9b742e8720a3221f884fa6511b245a712c7cdb
leksGo <Gan-96@yandex.ru>
2022-11-05 21:49:42 +0300

    add forgot password page and reset password page

--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -1,10 +1,42 @@
-import type { FC } from 'react';
+import { useMemo } from "react";
+import type { FC, ChangeEvent } from 'react';
+
+import AuthForm from "../../components/auth/auth-form/auth-form";
+import { InputType } from "../../models/auth-form";
 
 const ForgotPassword: FC = () => {
-    return (
-        <div>
+    const submitButton = useMemo(() => ({
+        title: 'Восстановить',
+        onClick: () => null
+    }), []);
 
-        </div>
+    const inputs = useMemo(() => [
+        {
+            type: InputType.Email,
+            inputProps: {
+                name: 'email',
+                placeholder: 'Укажите e-mail',
+                value: "",
+                onChange: (e: ChangeEvent<HTMLInputElement>) => null
+            }
+        }
+    ], []);
+
+    const linkRows = useMemo(() => [
+        {
+            paragraphText: 'Вспомнили пароль?',
+            buttonText: 'Войти',
+            onClick: () => null
+        }
+    ], []);
+
+    return (
+        <AuthForm
+            title="Восстановление пароля"
+            submitButton={submitButton}
+            inputs={inputs}
+            linkRows={linkRows}
+        />
     );
 };
 
--- src/pages/reset-password/reset-password.tsx
+++ src/pages/reset-password/reset-password.tsx
@@ -1,10 +1,51 @@
-import type { FC } from 'react';
+import { useMemo } from "react";
+import type { FC, ChangeEvent } from 'react';
+
+import AuthForm from "../../components/auth/auth-form/auth-form";
+import { InputType } from "../../models/auth-form";
 
 const ResetPassword: FC = () => {
-    return (
-        <div>
+    const submitButton = useMemo(() => ({
+        title: 'Сохранить',
+        onClick: () => null
+    }), []);
 
-        </div>
+    const inputs = useMemo(() => [
+        {
+            type: InputType.Password,
+            inputProps: {
+                name: 'password',
+                placeholder: 'Введите новый пароль',
+                value: "",
+                onChange: (e: ChangeEvent<HTMLInputElement>) => null
+            }
+        },
+        {
+            type: InputType.Default,
+            inputProps: {
+                name: 'code',
+                placeholder: 'Введите код из письма',
+                value: "",
+                onChange: (e: ChangeEvent<HTMLInputElement>) => null
+            }
+        }
+    ], []);
+
+    const linkRows = useMemo(() => [
+        {
+            paragraphText: 'Вспомнили пароль?',
+            buttonText: 'Войти',
+            onClick: () => null
+        }
+    ], []);
+
+    return (
+        <AuthForm
+            title="Восстановление пароля"
+            submitButton={submitButton}
+            inputs={inputs}
+            linkRows={linkRows}
+        />
     );
 };
 
