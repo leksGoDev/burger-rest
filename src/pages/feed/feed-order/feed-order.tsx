b55ff6e9e939d76e59497a3fd724b02776ae9738
leksGo <Gan-96@yandex.ru>
2022-12-12 22:24:19 +0300

    add feed order page

--- src/pages/feed/feed-order/feed-order.tsx
+++ src/pages/feed/feed-order/feed-order.tsx
@@ -1,11 +1,27 @@
+import { useEffect } from "react";
 import type { FC } from 'react';
 
+import styles from "./feed-order.module.css";
+import FeedOrderDetails from "../../../components/feed/feed-order-details/feed-order-details";
+import { useAppDispatch, useAppSelector } from "../../../hooks";
+import { deleteOrderDetails } from "../../../services/store/slices/feed-order-details";
+
 const FeedOrder: FC = () => {
+    const { details } = useAppSelector(store => store.feedOrderDetails);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        return () => {
+            if (details) {
+                dispatch(deleteOrderDetails());
+            }
+        }
+    }, [dispatch, details]);
 
     return (
-        <div>
-
-        </div>
+        <main className={styles.content}>
+            <FeedOrderDetails />
+        </main>
     );
 };
 
