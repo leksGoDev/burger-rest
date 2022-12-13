3f1427e40359785677762bb97ffc9ec0989a72f5
leksGo <Gan-96@yandex.ru>
2022-12-13 22:03:23 +0300

    split feed order list styles

--- src/components/feed/feed-main/feed-order-list/feed-order-list.tsx
+++ src/components/feed/feed-main/feed-order-list/feed-order-list.tsx
@@ -1,14 +1,16 @@
 import type { FC } from 'react';
+import { useLocation } from "react-router-dom";
 
 import styles from "./feed-order-list.module.css";
 import FeedOrderListCard from "./feed-order-list-card/feed-order-list-card";
 import { useSocketLastMessage } from "../../../../hooks";
 
 const FeedOrderList: FC = () => {
+    const { pathname } = useLocation();
     const data = useSocketLastMessage();
 
     return (
-        <ol className={styles.content}>
+        <ol className={pathname.indexOf("feed") !== -1 ? styles.inFeed : styles.inProfile}>
             {
                 data.orders.map((order, index) =>
                     <FeedOrderListCard key={index} {...order} />
--- src/pages/profile/profile-orders-history/profile-order-history.tsx
+++ src/pages/profile/profile-orders-history/profile-order-history.tsx
@@ -3,6 +3,7 @@ import type { FC } from 'react';
 
 import { WS_BASE_URL } from "../../../constants/api";
 import { getCookie } from "../../../services/api/cookie";
+import FeedOrderList from "../../../components/feed/feed-main/feed-order-list/feed-order-list";
 import { useAppDispatch } from "../../../hooks";
 import { startSocket } from "../../../services/store/slices/api/feed-socket-api";
 
@@ -15,9 +16,9 @@ const ProfileOrderHistory: FC = () => {
     }, [dispatch]);
 
     return (
-        <>
-
-        </>
+        <div style={{ marginTop: "44px" }}>
+            <FeedOrderList />
+        </div>
     );
 };
 
